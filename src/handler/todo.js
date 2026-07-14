9ef5166b9cbf6f9999e6fc45f90f0e9ec905ee8a
rodentskie <rodentskie@gmail.com>
2026-07-14 12:19:05 +0800

    fix: schema and insert

--- src/handler/todo.js
+++ src/handler/todo.js
@@ -1,11 +1,14 @@
+import { prisma } from "../lib/prisma.js";
 import { createTodoSchema } from "../models/todo.js";
 
-export function createTodo(req, res) {
+export async function createTodo(req, res) {
   const result = createTodoSchema.safeParse(req.body);
 
   if (!result.success) {
     return res.status(400).json({ error: result.error.flatten() });
   }
 
-  res.json(result.data);
+  const todo = await prisma.todo.create({ data: result.data });
+
+  res.status(201).json(todo);
 }
