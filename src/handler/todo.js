6b172f67e8551e864d9f8a420bcb41286ea886b4
rodentskie <rodentskie@gmail.com>
2026-07-14 12:30:45 +0800

    feat: insert todo

--- src/handler/todo.js
+++ src/handler/todo.js
@@ -7,7 +7,15 @@ export async function createTodo(req, res) {
   if (!result.success) {
     return res.status(400).json({ error: result.error.flatten() });
   }
+  /*
+  const existingCount = await prisma.todo.count({
+    where: { title: result.data.title },
+  });
 
+  if (existingCount > 0) {
+    return res.status(409).json({ error: "Title already exist" });
+  }
+  */
   const todo = await prisma.todo.create({ data: result.data });
 
   res.status(201).json(todo);
--- src/models/todo.js
+++ src/models/todo.js
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const createTodoSchema = z.object({
-  title: z.string().min(1),
+  title: z.string({ error: "Title is required" }).min(1, "Title cannot be empty"),
   description: z.string().optional(),
 });
